bf89186a9cd49e0819a8818d16b1d5120d961d32
Marie Blakely <marieblakely@myyahoo.com>
2024-04-13 11:45:10 -0400

    create flower collector database

---
+++ data/flower-data.js
@@ -0,0 +1,7 @@
+const flowerCollector = [
+  {type: 'Rose', perennial: true, planted: true},
+  {type: 'Daisy', perennial: true, planted: false},
+  {type: 'Callalily', perennial: true, planted: true},
+  {type: 'Gardenia', perennial: true, planted: false},
+  {type: 'Hydrangea', perennial: true, planted: true},
+]
